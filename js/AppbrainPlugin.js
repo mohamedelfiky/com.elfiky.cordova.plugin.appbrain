ed66ad649a09c02b7306a0aa345b771b033ad175
mohamedelfiky <mohamed.elfikyy@yahoo.com>
2015-08-03 17:27:28 +0200

    change app name plugin

--- js/AppbrainPlugin.js
+++ js/AppbrainPlugin.js
@@ -4,11 +4,11 @@ var AppBrain = {
     return cordova.exec(successCallback, errorCallback, 'AppBrainPlugin', 'initSdk', []);
   },
 
-  showPopup: function (successCallback, errorCallback) {
+  showBanner: function (successCallback, errorCallback) {
     return cordova.exec(successCallback, errorCallback, 'AppBrainPlugin', 'showBanner', []);
   },
 
-  showPopup: function (successCallback, errorCallback) {
+  showInterstitial: function (successCallback, errorCallback) {
     return cordova.exec(successCallback, errorCallback, 'AppBrainPlugin', 'showInterstitial', []);
   }
 };
